d7651ef25b04762a4ae8c1fc0b15762a43a52982
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-13 07:31:47 +0600

    update db to insert prymary data

--- util/db.php
+++ util/db.php
@@ -11,8 +11,8 @@ function ikr_leaflet_js_db_connection() {
     // Table 1: ikr_leaflet_js_db
     $table_name = $wpdb->prefix . 'ikr_leaflet_js_db';
     if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") !== $table_name) {
+        // If the table doesn't exist, create it
         $charset_collate = $wpdb->get_charset_collate();
-
         $sql = "CREATE TABLE $table_name (
             id int(10) NOT NULL AUTO_INCREMENT,
             lat varchar(100) NOT NULL,
@@ -21,8 +21,6 @@ function ikr_leaflet_js_db_connection() {
             marker_id varchar(100) NOT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
-
-        // Execute the SQL query to create the table
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
     }
@@ -30,8 +28,8 @@ function ikr_leaflet_js_db_connection() {
     // Table 2: ikr_default_setting
     $table_two =  $wpdb->prefix . 'ikr_default_setting';
     if ($wpdb->get_var("SHOW TABLES LIKE '$table_two'") !== $table_two) {
+        // If the table doesn't exist, create it
         $charset_collate = $wpdb->get_charset_collate();
-
         $sql_2 = "CREATE TABLE $table_two (
             id INT(10) NOT NULL AUTO_INCREMENT,
             Latitude FLOAT NOT NULL DEFAULT '23.8536047088421',
@@ -42,15 +40,24 @@ function ikr_leaflet_js_db_connection() {
             other VARCHAR(100) DEFAULT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
-
-        // Execute the SQL query to create the table
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql_2);
-    }
+
+        // Add default row values
+        $wpdb->insert($table_two, array(
+            'Latitude' => 23.8536047088421,
+            'Longitude' => 89.24606323242189,
+            'zoom' => 12,
+            'width' => 50,
+            'height' => 50
+        ));
+    } 
 }
 
-// Call the function to create the tables
+// Call the function to create/update tables
 ikr_leaflet_js_db_connection();
+
+
 
 
 ?>
