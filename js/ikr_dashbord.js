ef1eb03943ed2bcac8166179d69c294e10f0d304
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-13 08:52:52 +0600

    featch  the default data on dashbord page

--- js/ikr_dashbord.js
+++ js/ikr_dashbord.js
@@ -4,11 +4,27 @@ document.addEventListener("DOMContentLoaded",() =>{
     const defaultSetting_form = document.getElementById('defaultSetting');
     // add submit event  listener to form
     console.log(defaultSetting_form);
+
+async function getDefault_data (){
+    
+    try{
+        const default_data= await fetchAjaxRequest(get_url.featchdata);
+    console.log(default_data);
+    }catch (err) {
+        console.log(err);
+    }
+}
+
+getDefault_data();
+
     defaultSetting_form.addEventListener("click",(ev) =>{
         ev.preventDefault();
+      
+      
+      
         fetchAjaxRequest(get_url.featchdata,(data) =>{
             console.log(data);
-        })
+        });
     });
  
 
