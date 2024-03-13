bbae0ab98404758eb9437f386f0fe82b448bb345
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-13 10:01:06 +0600

    add default valu and redirect to add new page if request success

--- js/ikr_dashbord.js
+++ js/ikr_dashbord.js
@@ -2,14 +2,28 @@
 
 document.addEventListener("DOMContentLoaded",() =>{
     const defaultSetting_form = document.getElementById('defaultSetting');
-    // add submit event  listener to form
-    console.log(defaultSetting_form);
 
+    const default_lat = document.getElementById('default_lat');
+    const default_lng = document.getElementById('default_lng');
+    const default_zoom = document.getElementById('default_zoom');
+    const default_width = document.getElementById('default_width');
+    const default_height = document.getElementById('default_height');
+    
+
+
+
+// get default setting data 
 async function getDefault_data (){
     
     try{
         const default_data= await fetchAjaxRequest(get_url.featchdata);
-    console.log(default_data);
+  default_data.map(d=>{
+    default_lat.value = d.Latitude; 
+    default_lng.value =d.Longitude; 
+    default_zoom.value = d.zoom;
+    default_width.value = d.width;
+    default_height.value =d.height;
+  });
     }catch (err) {
         console.log(err);
     }
@@ -17,14 +31,16 @@ async function getDefault_data (){
 
 getDefault_data();
 
-    defaultSetting_form.addEventListener("click",(ev) =>{
+    defaultSetting_form.addEventListener("submit",(ev) =>{
         ev.preventDefault();
       
+      makeAjaxRequestGlobal(defaultSetting_form,get_url.default_form,call =>{
+   if( call){
+     window.location.href = "admin.php?page=Add-New-Map";
+   }
+      });
       
-      
-        fetchAjaxRequest(get_url.featchdata,(data) =>{
-            console.log(data);
-        });
+ 
     });
  
 
