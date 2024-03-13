53670ef5d60b58d2dca79fd1c029d0310d47aa64
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-13 10:00:04 +0600

    add new input form for detail

--- views/ikr_from.php
+++ views/ikr_from.php
@@ -6,8 +6,21 @@
     <input   placeholder="lat"   id="latituide" name="lat" type="text" id="lat" >
     <label for="lng">lngitude</label>
     <input placeholder="lng"    id="langtuide" name="lng" type="text" id="lat" >
-    <label for="popupText">Popup Text</label>
-    <input type="text" id="popup_text" name="popup-text"placeholder="Enter your text herer" value="marker popup ">
+
+    
+    <label for="address"> Address</label>
+    <input type="text" name="address" placeholder="Address" id="address">
+
+    <label for="phone"> phone</label>
+    <input type="text" id="phone" placeholder="your phone number" name="phone">
+
+    
+
+    <label for="url"> url</label>
+    <input type="text" name="url" placeholder="Your site url" id="input_url">
+
+
+    <input type="email" id="popup_text" name="popup-text"placeholder="Enter your email here" value=" ">
     <input type="submit" value="Submit"/>
 </form>
 
