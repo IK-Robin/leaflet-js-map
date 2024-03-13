3a58395b4c6a17f422c6505a5c23fc33721f259e
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-13 10:29:01 +0600

    add detail in edit

--- views/ikr_edit_marker.php
+++ views/ikr_edit_marker.php
@@ -9,9 +9,24 @@
         <label for="lng">lngitude</label>
         <input placeholder="lng" id="langtuide_edit" name="lng" type="text" id="lat">
 
-        <label for="popupText">Popup Text</label>
-        <input type="text" id="popup_text_edit" name="popup-text" placeholder="Enter your text herer" value="marker popup ">
+        
+    
+    <label for="address"> Address</label>
+    <input type="text" name="address" placeholder="Address" id="address_edit">
 
+    <label for="phone"> phone</label>
+    <input type="text" id="phone_edit" placeholder="your phone number" name="phone">
+
+    
+
+    <label for="url"> url</label>
+    <input type="text" name="url" placeholder="Your site url" id="input_url_edit">
+
+
+    <input type="email" id="popup_text_edit" name="popup-text"placeholder="Enter your email here" value=" ">
+
+
+        
         <input type="submit" value="Submit" />
     </form>
 </div>
