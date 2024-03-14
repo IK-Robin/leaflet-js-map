8b43e264d3f6ebad91f65bc96c535508577e10a0
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-14 13:07:34 +0600

    add a class

--- views/ikr_from.php
+++ views/ikr_from.php
@@ -1,30 +1,53 @@
+<div id="submit_form" class="submit_form">
+<a class="editClose_btn" id="closeB" href="#close">×</a>
 <form action="" id="ikr_map_form">
 
-    <input type="hidden" name="mapHiddenId" id="maphiddenId_add">
+<input type="hidden" name="mapHiddenId" id="maphiddenId_add">
 
-    <label for="lat">Latitude</label>
-    <input   placeholder="lat"   id="latituide" name="lat" type="text" id="lat" >
-    <label for="lng">lngitude</label>
-    <input placeholder="lng"    id="langtuide" name="lng" type="text" id="lat" >
+<div class="align_inp" >
+<label for="lat">Latitude</label>
+<input   placeholder="lat"   id="latituide" name="lat" type="text" id="lat" >
 
-    
-    <label for="address"> Address</label>
-    <input type="text" name="address" placeholder="Address" id="address">
+</div>
 
-    <label for="phone"> phone</label>
-    <input type="text" id="phone" placeholder="your phone number" name="phone">
+<div class="align_inp" >
+<label for="lng">lngitude</label>
+<input placeholder="lng"    id="langtuide" name="lng" type="text" id="lat" >
 
-    
+</div>
+<div class="align_inp" >
+<label for="address"> Address</label>
+<input type="text" name="address" placeholder="Address" id="address">
 
-    <input type="email" id="email" name="email"placeholder="Enter your email here" value=" ">
+</div>
+<div class="align_inp" >
 
-    <label for="url"> url</label>
-    <input type="text" name="url" placeholder="Your site url" id="input_url">
+<label for="phone"> phone</label>
+<input type="text" id="phone" placeholder="your phone number" name="phone">
 
-    <input type="submit" value="Submit"/>
+</div>
+<div class="align_inp" >
+
+<input type="email" id="email" name="email"placeholder="Enter your email here" value=" ">
+</div>
+
+<div class="align_inp" >
+<label for="url"> url</label>
+<input type="text" name="url" placeholder="Your site url" id="input_url">
+</div>
+
+
+<div class="align_inp" >
+<input type="submit" value="Submit"/>
+</div>
+
+
+
+
 </form>
 
 
 <form id="deletemarker_form" action=""> 
-    <input id="marker_id" type="hidden" name="markerID">
+<input id="marker_id" type="hidden" name="markerID">
 </form>
+</div>
