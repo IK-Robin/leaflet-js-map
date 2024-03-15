9e498abc0ff2cfad42841e1f2836d40732baeffd
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-15 08:46:31 +0600

    try to move the map in wp content

--- util/pages_func.php
+++ util/pages_func.php
@@ -16,26 +16,8 @@ function ikr_leafletjs_map()
 
 }
 
-function ikr_leaflet_Add_New_Map()
-{
 
-    ?>
-        <?php include_once ROBIN_DIR_PATH . './views/ikr_leaflet_main_page.php'; ?>
-    <div class="ikr_map_container">
-        <div class="ikr_first_side">
-            <div id="map"> </div>
-        </div>
-        <div class="ikr_second_side">
 
-            <?php include ROBIN_DIR_PATH . '/views/ikr_from.php' ?>
-
-            <?php include_once ROBIN_DIR_PATH . './views/ikr_edit_marker.php' ?>
-
-        </div>
-    </div>
-    <!-- add input form    -->
-    <?php
-}
 
 
 function ikr_leaflet_shortcode()
