4ba11f5be2d4835a57f3972ab71d59b30b99f371
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-15 09:38:25 +0600

    put the conten in wp content

--- util/pages_func.php
+++ util/pages_func.php
@@ -17,6 +17,29 @@ function ikr_leafletjs_map()
 }
 
 
+function ikr_leaflet_Add_New_Map()
+{
+
+    ?>
+
+<div class="ikr_map_container">
+        <?php include_once ROBIN_DIR_PATH . './views/ikr_leaflet_main_page.php'; ?>
+        <div class="ikr_map_content">
+        <div class="ikr_first_side">
+            <div id="map"> </div>
+        </div>
+        <div class="ikr_second_side">
+
+            <?php include ROBIN_DIR_PATH . '/views/ikr_from.php' ?>
+
+            <?php include_once ROBIN_DIR_PATH . './views/ikr_edit_marker.php' ?>
+
+        </div>
+        </div>
+    </div>
+    <!-- add input form    -->
+    <?php
+}
 
 
 
