d04d6030e2fb3748f381be30004262fb1e923874
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-15 09:38:54 +0600

    put the conten in pages_func

--- functions.php
+++ functions.php
@@ -23,26 +23,6 @@ include_once ROBIN_DIR_PATH . './util/enqueue_script.php';
 include_once ROBIN_DIR_PATH .'util/pages_func.php';
 
 
-function ikr_leaflet_Add_New_Map()
-{
-
-    ?>
-        <?php include_once ROBIN_DIR_PATH . './views/ikr_leaflet_main_page.php'; ?>
-    <div class="ikr_map_container">
-        <div class="ikr_first_side">
-            <div id="map"> </div>
-        </div>
-        <div class="ikr_second_side">
-
-            <?php include ROBIN_DIR_PATH . '/views/ikr_from.php' ?>
-
-            <?php include_once ROBIN_DIR_PATH . './views/ikr_edit_marker.php' ?>
-
-        </div>
-    </div>
-    <!-- add input form    -->
-    <?php
-}
 /*********************
  * add database related code  here
  ********************/
