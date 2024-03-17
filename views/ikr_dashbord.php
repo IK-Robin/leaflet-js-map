806b5c512aef401dd73efce5aee66abf78a05c3f
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-17 10:56:37 +0600

    update dashbord text

--- views/ikr_dashbord.php
+++ views/ikr_dashbord.php
@@ -13,38 +13,38 @@
             <div class="default_f_section">
                <label for="default_lat"> Default Latitude </label> 
                <input type="text" name="default_lat"  id ="default_lat">
-               <p>Default longitude for maps. You can also change this for each map </p>
+               <p>The default Latitude determines the starting viewpoint on maps when the page loads. You can also modify the Latitude to change where the map centers when you first open it.</p>
             </div>
 
             <div class="default_f_section">
               <label for="default_lng">Default Longitude: </label>
               <input type="text" name="default_lng" id = "default_lng">
-             <p> Default zoom for maps. You can also change this for each map </p>
+             <p> The default longitude determines the starting viewpoint on maps when the page loads. You can also modify the longitude to change where the map centers when you first open it. </p>
             </div>
             <div class="default_f_section">
               <label for="default_zoom">Default Zoom: </label>
               <input type="number"   min="1" max="18" name="default_zoom" id = "default_zoom">
-            <p>   Default Zoom for maps. Values can include 1 to 18  You can also change this for each map </p>
+            <p>  The default zoom for maps sets how close or far away the map appears when you first open it. You can adjust this zoom level for each map, with values ranging from 1 to 18, allowing you to customize the view according to your preference. </p>
             </div>
             
             <div class="default_f_section">
               <label for="default_width">Default Width: </label>
               <input type="number" min="10" max="100"  name="default_width" id = "default_width">
-            <p>   Default Width for maps. Values can include in %. You can also change this for each map Width. </p>
+            <p>  The default width for maps establishes their initial size, with values expressed in percentages. You can also individually adjust the width for each map according to your preferences. </p>
             </div>
             
             <div class="default_f_section">
               <label for="default_height">Default Height: </label>
               <input type="number" min="100"   name="default_height" id="default_height">
 
-            <p>   Default Height for maps. Values can include in %. You can also change this for each map Height. </p>
+            <p>   The default Height for maps establishes their initial size, with values expressed in percentages. You can also individually adjust the Height for each map according to your preferences. </p>
             </div>
             
             <div class="default_f_section">
               <label for="default_link">Default url: </label>
               <input type="text"  name="default_link" id="default_link">
 
-            <p>   Default url for maps. Values can include any link such as your website link. You can also change this for each map url. </p>
+            <p>   The default URL for maps specifies the initial link, which can be any web address, like your website. You have the option to customize this URL for each map to direct users to different destinations as needed. </p>
             </div>
 
 
