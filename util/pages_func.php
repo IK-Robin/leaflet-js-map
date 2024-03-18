8b9d4c1432c1c0e0b6c72ebc5ef1b9acdaf268c4
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-18 09:42:46 +0600

    problem is from not add in wp admin page

--- util/pages_func.php
+++ util/pages_func.php
@@ -9,41 +9,35 @@
 
 function ikr_leafletjs_map()
 {
-
     ?>
-    <?php include_once ROBIN_DIR_PATH . './views/ikr_dashbord.php'; ?>
-<?php
+    <div class="ikr_map_container">
+        <?php include_once ROBIN_DIR_PATH . './views/ikr_leaflet_main_page.php'; ?>
+        <div class="ikr_map_content">
+            <div class="ikr_first_side">
+                <div id="map"> </div>
+            </div>
+            <div class="ikr_second_side">
+                <?php include ROBIN_DIR_PATH . '/views/ikr_from.php' ?>
+                <?php include_once ROBIN_DIR_PATH . './views/ikr_edit_marker.php' ?>
+               
+            </div>
+        </div>
 
+        <!-- Add input form -->
+      
+
+    </div>
+    <?php
 }
+
+
 
 
 function ikr_leaflet_Add_New_Map()
 {
-
-    ?>
-
-<div class="ikr_map_container">
-        <?php include_once ROBIN_DIR_PATH . './views/ikr_leaflet_main_page.php'; ?>
-        <div class="ikr_map_content">
-        <div class="ikr_first_side">
-            <div id="map"> </div>
-        </div>
-        <div class="ikr_second_side">
-
-            <?php include ROBIN_DIR_PATH . '/views/ikr_from.php' ?>
-
-            <?php include_once ROBIN_DIR_PATH . './views/ikr_edit_marker.php' ?>
-
-        </div>
-        </div>
-
-        
-        <!-- show map documentation -->
-        <?php include_once ROBIN_DIR_PATH . './views/documentation.php';?>
-
-    </div>
-    <!-- add input form    -->
-    <?php
+?> 
+    <?php include_once ROBIN_DIR_PATH . './views/ikr_dashbord.php'; ?>
+<?php
 }
 
 
