b852fed4d7dfab0c8c3e43b5bbf118d3128df700
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-18 09:42:18 +0600

    add document

--- views/documentation.php
+++ views/documentation.php
@@ -1,4 +1,33 @@
 <div class="documantation">
-    <h1>Documentation</h1>
-    
+    <h1>How to Edit</h1>
+
+    <h3>Add Custom Marker Postion</h3>
+    <div><strong>You can add new markers by using following steps:</strong>
+        <ol>
+            <li> Please add the longitude and latitude, along with any other necessary details, then click "Add New."
+            </li>
+            <li>
+                Or Simply click the "Add New Marker" button located in the top left corner, then click on the map to
+                place a marker.</li>
+        </ol>
+    </div>
+
+
+    <h3>Edit The Map Detail </h3>
+    <strong>  You can edit the map details by following these steps:</strong>
+    <ol>
+        <li>Click on any marker of the map . A pop up will appear showing all the information related to that particular marker.
+        <li> Here you can see Two button  "Edit" and "Delete".</li>
+   
+        <li>
+            Now Click on Edit  Button. It will open a pop up window where you can make changes in all fields of that particular then click Update.
+        </li>
+        <li><strong>Delete marker</strong> Click on Delete marker.</li>
+        </li>
+    </ol>
+        <h3>Change the marker Postion </h3>
+        <p>To change the marker Postion Simply drage the marker. </p>
+        <p>Or click the marekr it open a popup then click edit it  open  a pop up window top right corner where you can make changes latitude and longitude fields of that particular then click Update .</p>
+
+        
 </div>
