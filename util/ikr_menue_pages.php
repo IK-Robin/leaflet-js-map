48740b6455f4f40f2b0b8e2f729f741b47092092
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-18 09:44:08 +0600

    change slug and name

--- util/ikr_menue_pages.php
+++ util/ikr_menue_pages.php
@@ -14,7 +14,7 @@
    add_submenu_page('wp-store-locator-map', "Dashboard", "Dashboard", 'manage_options', 'wp-store-locator-map', 'ikr_leafletjs_map');
  
    // Add submenu for 'All Maps'
-   add_submenu_page('wp-store-locator-map', 'Add New Map', 'Add New Map', 'manage_options', 'Add-New-Map', 'ikr_leaflet_Add_New_Map');
+   add_submenu_page('wp-store-locator-map', 'Setting', 'Setting', 'manage_options', 'setting', 'ikr_leaflet_Add_New_Map');
    add_submenu_page('wp-store-locator-map', 'Shortcode', 'Shortcode', 'manage_options', 'Shortcode', 'ikr_leaflet_shortcode');
  }
 
