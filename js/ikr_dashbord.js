77ccf5535e7030b07bf71f22818fd1bc62043009
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-23 11:37:22 +0600

    set the zoom function filter

--- js/ikr_dashbord.js
+++ js/ikr_dashbord.js
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded",() =>{
   
 const customZoom = document.getElementById('customZoom');
 const autoZoom = document.getElementById('autoZoom');
+const zoomOption  = document.getElementById('zoomOption');
 
-console.log(customZoom);
  
 
 
@@ -40,12 +40,46 @@ default_data.map(d=>{
 
 getDefault_data();
 
+
+customZoom.addEventListener('click', (e) => {
+    // Set the value of customZoom
+    customZoom.value = 'custom_zoom';
+
+
+    // Clear the value of autoZoom and uncheck it
+    autoZoom.value = '';
+    autoZoom.checked = false;
+
+    // Check customZoom
+    if (!customZoom.checked) {
+        customZoom.checked = true;
+    }
+    customZoom.value= 'custom_zoom';
+});
+
+autoZoom.addEventListener('click', (e) => {
+    // Set the value of autoZoom
+    autoZoom.value = 'auto_zoom';
+
+
+    // Clear the value of customZoom and uncheck it
+    customZoom.value = '';
+    customZoom.checked = false;
+
+    // Check autoZoom
+    if (!autoZoom.checked) {
+        autoZoom.checked = true;
+    }
+    zoomOption.value= 'auto_zoom';
+});
+
+
   defaultSetting_form.addEventListener("submit",(ev) =>{
       ev.preventDefault();
     
     makeAjaxRequestGlobal(defaultSetting_form,get_default.default_form,call =>{
  if( call){
-   window.location.reload();
+  //  window.location.reload();
  }
     });
     
