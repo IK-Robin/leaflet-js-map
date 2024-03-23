14f64c98b55e07b32b606312e952b31d84862d21
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-23 12:42:14 +0600

    remove statastic checked input

--- views/ikr_dashbord.php
+++ views/ikr_dashbord.php
@@ -16,7 +16,7 @@
           <input type="hidden" name="zoomOption" id="zoomOption" value="auto_zoom">
           <div class="form-check">
             <label class="form-check-label">
-              <input type="checkbox" class="form-check-input" name="customZoom" id="customZoom" value="" checked>
+              <input type="checkbox" class="form-check-input" name="customZoom" id="customZoom" value="" >
               Custom zoom
             </label>
           </div>
