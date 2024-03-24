b1c81018239899901fed81d80086171b7480a224
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-03-24 09:28:55 +0600

    update text

--- views/ikr_dashbord.php
+++ views/ikr_dashbord.php
@@ -13,7 +13,7 @@
       <div id="defaultSettings">
 
         <div class="default_f_section">
-        <p> Add (latitude and longitude) to set the center point of the map view. </p>
+   
           <input type="hidden" name="zoomOption" id="zoomOption" value="auto_zoom">
           <div class="form-check">
             <label class="form-check-label">
@@ -27,36 +27,39 @@
               auto set map center view
             </label>
           </div>
-
+          <p>Add custom zoom or Auto Zoom to set the center point of the map view. </p>
           
         </div>
         <div class="default_f_section">
+
+      
           <label for="default_width"> Width: </label>
           <input type="number" min="10" max="100" name="default_width" id="default_width">
 
-          <label for="default_height"> Height: </label>
+          <label for="default_height"> Height:  </label>
           <input type="number" min="100" name="default_height" id="default_height">
 
-          <p> The default width for maps establishes their initial size, with values expressed in percentages. You can
-            also individually adjust the width for each map according to your preferences. </p>
+          <p> Aspect Ration of the map value should be Width:  in " % " height in " px " </p>
         </div>
 
 
 
         <div class="default_f_section showOption">
+       
           <label for="default_lat"> Latitude </label>
           <input type="text" name="default_lat" id="default_lat">
           <label for="default_lng"> Longitude: </label>
           <input type="text" name="default_lng" id="default_lng">
-          <p>The default Latitude Longitude determines the starting viewpoint on maps when the page loads. You can also
-            modify the Latitude to change where the map centers when you first open it.</p>
+          <p>       Add (latitude and longitude) to set to set the center point of the map view.</p>
+     
         </div>
 
 
         <div class="default_f_section showOption">
-          <label for="default_zoom"> Zoom: </label>
+      
+          <label class="zoomLavel" for="default_zoom"> Zoom: </label>
           <input type="number" min="1" max="18" name="default_zoom" id="default_zoom">
-          <p> The default zoom for maps sets how close or far away the map appears when you first open it. You can
+          <p class="zoomP"> The default zoom for maps sets how close or far away the map appears when you first open it. You can
             adjust this zoom level for each map, with values ranging from 1 to 18, allowing you to customize the view
             according to your preference. </p>
         </div>
