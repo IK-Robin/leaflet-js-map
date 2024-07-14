8e602b1037a29a1ab754e0aa49113399cdb7281a
Robin <62151722+IK-Robin@users.noreply.github.com>
2024-07-13 23:01:48 -0700

    solved db prepair statment problem

--- util/db.php
+++ util/db.php
@@ -3,17 +3,17 @@
  * This file is for creating a database table for Leaflet.js map
 ****/
 // Create database tables
-
 function ikr_leaflet_js_db_connection() {
     global $wpdb;
 
-    // Table 1: ikr_leaflet_js_db
+    // Table prefixes
     $table_name_1 = $wpdb->prefix . 'ikr_leaflet_js_db';
+    $table_name_2 = $wpdb->prefix . 'ikr_default_setting';
     $charset_collate = $wpdb->get_charset_collate();
 
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name_1'") != $table_name_1) {
-        // If the table doesn't exist, create it
-        $sql_1 = "CREATE TABLE $table_name_1 (
+    // Create table 1 if it doesn't exist
+    if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name_1'" ) != $table_name_1 ) {
+        $sql = "CREATE TABLE $table_name_1 (
             id INT(10) NOT NULL AUTO_INCREMENT,
             lat VARCHAR(100) NOT NULL,
             lng VARCHAR(100) NOT NULL,
@@ -22,45 +22,29 @@ function ikr_leaflet_js_db_connection() {
             email VARCHAR(100) NOT NULL,
             urls VARCHAR(1000) NOT NULL,
             marker_id VARCHAR(100) NOT NULL,
-         
             PRIMARY KEY (id)
         ) $charset_collate;";
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql_1);
-         // Add default row values
-         $wpdb->insert($table_name_1, array(
-            'lat' => 23.8536047088421,
-            'lng' => 89.24606323242189,
-            'address' => 2,
-            'phone' => 100,
-            'email' => 300,
-            'urls' => 'https://hamjaiu.com',
-            'marker_id' => '88759345',
-        ));
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+
+        // Insert default data using prepared statements
+        $wpdb->insert(
+            $table_name_1,
+            array(
+                'lat' => 23.8536047088421,
+                'lng' => 89.24606323242189,
+                'address' => '2',
+                'phone' => '100',
+                'email' => '300',
+                'urls' => 'https://hamjaiu.com',
+                'marker_id' => '88759345',
+            )
+        );
     }
 
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name_1'") == $table_name_1) {
-        // If the table already exists, add the new fields if they don't exist
-        $fields_to_add = array('id', 'lat', 'lng', 'address', 'phone','email','urls','marker_id');
-        
-        foreach ($fields_to_add as $field) {
-            $field_exists = $wpdb->get_var("SHOW COLUMNS FROM $table_name_1 LIKE '$field'");
-            if (!$field_exists) {
-                $alter_sql = "ALTER TABLE $table_name_1 ADD $field VARCHAR(100) NOT NULL AFTER marker_id";
-                $wpdb->query($alter_sql);
-            }
-        }
-    }
-    
-    
-
-    // Table 2: ikr_default_setting
-    $table_name_2 =  $wpdb->prefix . 'ikr_default_setting';
-
-
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name_2'") != $table_name_2) {
-        // If the table doesn't exist, create it
-        $sql_2 = "CREATE TABLE $table_name_2 (
+    // Create table 2 if it doesn't exist
+    if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name_2'" ) != $table_name_2 ) {
+        $sql = "CREATE TABLE $table_name_2 (
             id INT(10) NOT NULL AUTO_INCREMENT,
             Latitude FLOAT NOT NULL DEFAULT 23.8536047088421,
             Longitude FLOAT NOT NULL DEFAULT 89.24606323242189,
@@ -70,32 +54,21 @@ function ikr_leaflet_js_db_connection() {
             zoom_option VARCHAR(100) DEFAULT NULL,
             PRIMARY KEY (id)
         ) $charset_collate;";
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql_2);
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
 
-        // Add default row values
-        $wpdb->insert($table_name_2, array(
-            'Latitude' => 23.8536047088421,
-            'Longitude' => 89.24606323242189,
-            'zoom' => 2,
-            'width' => 100,
-            'height' => 300,
-            'zoom_option' => 'auto_zoom',
-        ));
-    }
-
-
-    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name_2'") == $table_name_2) {
-        // If the table already exists, add the new fields if they don't exist
-        $fields_to_add = array('id', 'Latitude', 'Longitude', 'zoom', 'width','height','zoom_option');
-        
-        foreach ($fields_to_add as $field) {
-            $field_exists = $wpdb->get_var("SHOW COLUMNS FROM $table_name_2 LIKE '$field'");
-            if (!$field_exists) {
-                $alter_sql = "ALTER TABLE $table_name_2 ADD $field VARCHAR(100) NOT NULL AFTER marker_id";
-                $wpdb->query($alter_sql);
-            }
-        }
+        // Insert default data using prepared statements
+        $wpdb->insert(
+            $table_name_2,
+            array(
+                'Latitude' => 23.8536047088421,
+                'Longitude' => 89.24606323242189,
+                'zoom' => 2,
+                'width' => 100,
+                'height' => 300,
+                'zoom_option' => 'auto_zoom',
+            )
+        );
     }
 }
 
